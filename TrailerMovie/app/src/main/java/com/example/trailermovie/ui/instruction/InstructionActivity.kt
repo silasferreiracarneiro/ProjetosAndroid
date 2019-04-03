a8d480b10c9d2567aeaf6895865b0d6fabcf05ff
silasferreiracarneiro <silasferreira96@hotmail.com>
2019-04-02 23:26:58 -0300

    Feito o carrossel da pagina de introdução

--- TrailerMovie/app/src/main/java/com/example/trailermovie/ui/instruction/InstructionActivity.kt
+++ TrailerMovie/app/src/main/java/com/example/trailermovie/ui/instruction/InstructionActivity.kt
@@ -3,6 +3,7 @@ package com.example.trailermovie.ui.instruction
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trailermovie.R
+import kotlinx.android.synthetic.main.activity_instruction.*
 
 
 class InstructionActivity : AppCompatActivity() {
@@ -11,5 +12,9 @@ class InstructionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instruction)
 
+        var adapter = SlideAdapter(arrayListOf(), this)
+        viewPager.adapter = adapter
+        adapter.addIndicator(0)
+        viewPager.addOnPageChangeListener(adapter.litener)
     }
 }
